74cf3052b755e3739c2b6fcba2f8b6515064bccf
gingin77 <ginniehench@gmail.com>
2017-09-11 20:30:55 -0400

    I eliminated 2 lines that reassigned variables transformed with .chars. Instead, the .chars is included into the .zip >> .map >> .count operation

--- hamming.rb
+++ hamming.rb
@@ -1,31 +1,10 @@
 class Hamming
     def self.compute(top_strand, bottom_strand)
       raise ArgumentError if top_strand.length != bottom_strand.length
-      top_strand = top_strand.chars
-      bottom_strand = bottom_strand.chars
-      top_strand.zip(bottom_strand).collect{|x, y| x == y}.count(false)
+      top_strand.chars.zip(bottom_strand.chars).collect{|x, y| x == y}.count(false)
     end
 end
 
 module BookKeeping
   VERSION = 3
 end
-
-
-
-# arr = [1, 2, 3, 4, 5]
-# arr.each { |a| print a -= 10, " " }
-# # prints: -9 -8 -7 -6 -5
-# #=> [1, 2, 3, 4, 5]
-
-
-
-# if_this_is_a_true_value ? then_the_result_is_this : else_it_is_this
-#
-#
-# puts (if 1 then 2 else 3 end) # => 2
-#
-# puts 1 ? 2 : 3                # => 2
-#
-# x = if 1 then 2 else 3 end
-# puts x                        # => 2
